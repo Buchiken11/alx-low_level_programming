d6166b2ccf04b4e0b2382e3a2a9e164edbcb319d
Buchiken11 <buchiken025@gmail.com>
2023-02-18 04:40:53 -0800

    doing it

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - Entry point a program that printf functions
- * Return: Always (0)
+ * main - A rogram that prints f function
+ * Return: Always 0 (success)
  */
 int main(void)
 {
