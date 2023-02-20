3fd911caf9623b6c110a85ffe490f136b9ed9545
Buchiken11 <buchiken025@gmail.com>
2023-02-20 05:55:56 -0800

    no understanding

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * a program that print alphabets in lowercase
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n = 97;
+	while (n <= 122)
+		putchar(n);
+		n++;
+	return (0);
+}
+
