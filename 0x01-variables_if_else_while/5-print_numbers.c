b7e083bdccd4b50ce66098659bf8df1c111d440a
Buchiken11 <buchiken025@gmail.com>
2023-02-23 07:12:29 -0800

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char n = 10;
+
+	for (n = '0'; n <= '10'; n++)
+	printf(n'\n');
+	return (0);
+}
