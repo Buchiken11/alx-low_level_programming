5e5e82b5a77da935345bc2990d2c1dd4877cc30d
Buchiken11 <buchiken025@gmail.com>
2023-08-11 00:08:20 +0100

    4-puts.c printf can do

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /*
  * main-Entry point
- *
+ * Description: A programm that prints a string to the standard output
  * Return:Always Success(0)
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
