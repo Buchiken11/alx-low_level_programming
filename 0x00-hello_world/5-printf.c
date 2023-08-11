e6db2162c5aed794bf9842dc6d37d8cdbe12fc4c
Buchiken11 <buchiken025@gmail.com>
2023-08-11 01:06:41 +0100

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point A function that prints with printf to the stanfard output
+ * main - Entry point A function that prints with printf to the stanfard outputs
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-    return (0);
+	return (0);
 }
