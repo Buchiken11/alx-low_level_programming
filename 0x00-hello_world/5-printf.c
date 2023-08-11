ac821cbc5a2f0b5635a7f079dbb848d1156e9c49
Buchiken11 <buchiken025@gmail.com>
2023-08-11 01:01:53 +0100

    4-puts.c printf can do

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point A function that prints with printf to the stanfard output
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+    return (0);
+}
