a621bf42cea45bf83cf6303e3bf8467d9c8896df
Buchiken11 <buchiken025@gmail.com>
2023-08-12 23:19:02 +0100

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main-Entry point A function that prints alpabet in lower an upper case with putchar
+ *
+ * Return: Always 0. (Success)
+ */
+int main(void)
+{
+	char alpha;
+	char upper;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	for (upper = 'A'; upper <= 'Z'; upper++)
+	{
+		putchar(upper);
+	}
+		putchar('\n');
+}
+
