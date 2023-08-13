9cc526ff0fb40d87a75c957d6a8661bb729dfb0b
Buchiken11 <buchiken025@gmail.com>
2023-08-13 16:15:09 +0100

    100-print_comb3 hard project

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,50 +3,33 @@
 
 /**
 
- * main - prints all possible different combinations of two digits
-
+ * main -Entry poont A function that  prints all possible different combinations of two digits
+ *
  * Return: ALways 0 (Success)
-
  */
 
 int main(void)
 
 {
 
-        int n, m;
+	int n, m;
 
-
-        for (n = 48; n <= 56; n++)
-
-        {
-
-                for (m = 49; m <= 57; m++)
-
-                {
-
-                        if (m > n)
-
-                        {
-
-                                putchar(n);
-
-                                putchar(m);
-
-                                if (n != 56 || m != 57)
-
-                                {
-
-                                        putchar(',');
-
-                                        putchar(' ');
-
-                                }
-
-                        }
-
+	 for (n = 48; n <= 56; n++)
+	{
+	for (m = 49; m <= 57; m++)
+	{
+		if (m > n)
+		{
+			putchar(n);
+			putchar(m);
+			if (n != 56 || m != 57)
+		{
+			putchar(',');
+			putchar(' ');
                 }
-
-        }
+                }
+                }
+	}
 
         putchar('\n');
 
