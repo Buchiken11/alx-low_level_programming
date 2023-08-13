3ba5327f949eb4ed14dec49858aa89a7d026c41c
Buchiken11 <buchiken025@gmail.com>
2023-08-13 13:33:03 +0100

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-Entry point Afunction that prints base 10 numbers 0-10
+ *
+ * Return: Always 0. (Success)
+ */
+int main(void)
+{
+	int p = 48;
+
+	for (p = 48; p <=57; p++)
+	{
+		putchar('p');
+	}
+		putchar('\n');
+		return (0);
+}
