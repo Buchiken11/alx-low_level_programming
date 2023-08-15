cf3aea0d2e0e2062538f9d122194488c9b1118d0
Buchiken11 <buchiken025@gmail.com>
2023-08-15 22:12:21 +0100

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha-prints alpabetic character upper case or lowercase
+ *
+ * Return: 1 on success
+ */
+int _isalpha(int c)
+
+{
+
+        return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+
+}
