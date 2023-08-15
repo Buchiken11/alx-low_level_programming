13eb184ccb2f0ccdc8ea9ca77b3a39c848a59968
Buchiken11 <buchiken025@gmail.com>
2023-08-15 21:47:34 +0100

    3-islower.c confused about this

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * int _islower(int c)-function prints lowecase alphabet.
+ * _islower(int c)-function prints lowecase alphabet
  *
  * Return: 1 on success
  */
 int _islower(int c)
 {
 	
+
 	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
