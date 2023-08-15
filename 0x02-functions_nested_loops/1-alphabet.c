a537e8a973e0fd270e134c8350b7a5d1b3c6bfb5
Buchiken11 <buchiken025@gmail.com>
2023-08-15 16:10:22 +0100

    1-alphabet.c project

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * main-Entry point A function that prints alpabet with void print_alphabet(void)
+ * main-Entry point A function that prints alpabet in lower case
  *
  * Return: 0
  */
 void print_alphabet(void)
 {
 	char alpha;
+
 	{
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
-	_putchar(alpha);
-	_putchar('\n');
+		_putchar(alpha);
+		_putchar('\n');
 	}
 }
