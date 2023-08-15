d12a329c6634ef6fe6608a474ee727d07f72541b
Buchiken11 <buchiken025@gmail.com>
2023-08-15 16:02:51 +0100

    1-alphabet.c project

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main-Entry point A function that prints alpabet
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char alpha;
+	{
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	_putchar(alpha);
+	_putchar('\n');
+	}
+}
