7d7277827cc4b9518e7f0d37b1779f4302471429
Buchiken11 <buchiken025@gmail.com>
2023-08-17 08:07:09 +0100

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,19 +4,12 @@
  *
  * Return: 0.
  */
-void positive_or_negative(int i)
 
+int main(void)
 {
-	i = 0;
+	int i;
 
-	if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else if (i == 0)
-	{
-		printf("%d is zero\n", i);
-	}
-	else
-		printf("%d is negative\n", i);
+	i = 0;
+	positive_or_negative(i);
+	return (0);
 }
