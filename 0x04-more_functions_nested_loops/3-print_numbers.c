22a02e0e16ba3b533db385c10ae1d66647d5e9b5
Buchiken11 <buchiken025@gmail.com>
2023-08-17 20:43:45 +0100

    this function prints numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers-functions prints numbers 0 to 9
+ *
+ * Return: Always 0. (Success)
+ */
+
+void print_numbers(void)
+{
+	char alpha = 48;
+	while (alpha <= 57)
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+		_putchar('\n');
+}
+
+
