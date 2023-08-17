8ce4e857f1c1877835f8ddf89f4d14506f6e178b
Buchiken11 <buchiken025@gmail.com>
2023-08-17 19:27:55 +0100

    this fuction multiplies two intrgers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul-function multiply the two intrgers and return output
+ * @a: a is variable 1
+ * @b: b is variabl 2
+ * Return: result(a*b)
+ */
+
+int mul(int a, int b)
+{
+	int mul =(a*b);
+	return (mul);
+}
+
