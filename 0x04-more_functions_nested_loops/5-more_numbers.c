183b2defa3f2bbf9505c8839699a79c6ecf20bd8
Buchiken11 <buchiken025@gmail.com>
2023-08-18 12:10:15 +0100

    this function prints 1- 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,24 +1,25 @@
 #include "main.h"
+/**
+ * more_numbers-function prints 0-14 in ten lines
+ *
+ * Return: Void
+ */
 
 void more_numbers(void)
 {
-    int i = 0;
-
-    while (i < 10)
-    {
-        int j = 0;
-
-        while (j < 15)
-        {
-            if (j >= 10)
-                _putchar(j / 10 + '0');
-            _putchar(j % 10 + '0');
-
-            j++;
-        }
-
-        _putchar('\n');
-        i++;
-    }
+	int i = 0;
+	while (i < 10)
+	{
+		int j = 0;
+		while (j < 15)
+		{
+			if (j >= 10)
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
 
