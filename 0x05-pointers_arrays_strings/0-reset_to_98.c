eb4534f83925033e962666055778b8d305bfef1f
Buchiken11 <buchiken025@gmail.com>
2023-08-21 14:53:59 +0100

    inializing pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98-function prints the value and address of n
+ *@n: *n is a pointer variable
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
