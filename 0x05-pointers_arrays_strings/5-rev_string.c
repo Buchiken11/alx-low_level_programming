944c9b3b74a40ed518973dc79e428c73dd8e6aa6
Buchiken11 <buchiken025@gmail.com>
2023-08-21 18:09:42 +0100

    5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,7 +12,8 @@ void rev_string(char *s)
 
 	while (s[counter] != '\0')
 		counter++;
-	for (i = 0; i < counter; i++){
+	for (i = 0; i < counter; i++)
+	{
 		counter--;
 		rev = s[i];
 		s[i] = s[counter];
