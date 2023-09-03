c92b6f9d471f1170342352e2a4d8c9f44f85320f
Buchiken11 <buchiken025@gmail.com>
2023-09-03 11:27:05 +0100

    this prints arguments count

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * main-function prints number argument passed to the terminal
- * @argc: counter argument.
- * @argv: vector variable count
- * Return: 0 (Success).
+ *@argc: counter argument.
+ *@argv: vector variable count
+ * Return: Always 0 (Success).
  */
 
 int main(int argc, char *argv[])
 {
-
 	(void)argv;/*argv*/
 	{
 	printf("%d\n", argc-1);
