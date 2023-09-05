b6542e71e4ea27aa9a189364e1aa7cebede5e675
Buchiken11 <buchiken025@gmail.com>
2023-09-05 16:07:44 +0100

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array-fuction creates an array of characters 
+ * and initializes it to c.
+ * @size: size of unsigned itegers
+ * @sizema: size of array member
+ * @c: variable
+ * ptrf: pointer to be returned
+ * Return 0.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptrf;
+	unsigned int sizema;
+
+	ptrf = malloc(size * sizeof (char));
+	for (sizema = 0; sizema < size; sizema++)
+	{
+		ptrf[sizema] = c;
+	}
+	return (ptrf);
+	free (ptrf);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Learning dynamic memory allocation, using malloc to allocate certain amount of memory to a program at rumtime.
