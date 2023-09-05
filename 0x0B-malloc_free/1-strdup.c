371237b147fb494d971baeb1e7e988512173a106
Buchiken11 <buchiken025@gmail.com>
2023-09-05 22:10:16 +0100

    num 1 task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup-function allocates memory for a duplicated string
+ * @str: pointer to the string
+ * @n: interger counter variable
+ * @i: inter to loop through the string
+ * Return: pointer
+ */
+
+char *_strdup(char *str)
+
+{
+	char *buchi;
+	int n = 0;
+	int i = 0;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	for(; str[i] != '\0'; i++)
+	{
+		n++;
+	}
+	buchi = malloc(n*sizeof(char) + 1);
+	if (buchi == 0)
+	{
+		return (NULL);
+	}
+	i = 0;
+	for(i = 0; i <= n; i++)
+	{
+		buchi[i] = str[i];
+	}
+	return (buchi);
+}
