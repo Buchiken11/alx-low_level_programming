6f254a7718fbe44f0b55f0e881c6404fa14e4231
Buchiken11 <buchiken025@gmail.com>
2023-09-06 04:10:51 +0100

    3-alloc_grid.c creates memory for a 2d array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	for (x = 0; x < height; x++)
 	{
-		mee[x] = malloc(sizeof(int) * width);
+		ken[x] = malloc(sizeof(int) * width);
 		if (ken[x] == NULL)
 		{
 			for (; x >= 0; x--)
