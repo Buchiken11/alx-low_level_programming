fb900317817856d715c7a75d395488db14824956
Buchiken11 <buchiken025@gmail.com>
2023-09-12 18:11:34 +0100

    name of file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main-fuction prints the name of file
+ *
+ * Return 0.
+ */
+
 int main (void)
 {
 	char *program_name;
+
 	program_name = __FILE__;
 	{
 	printf("%s", program_name);
