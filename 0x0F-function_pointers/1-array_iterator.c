8b98cae9596b9616b19fe9aeb45eda17baec76db
Buchiken11 <buchiken025@gmail.com>
2023-09-14 06:09:40 +0100

    function pointer to array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,14 +6,14 @@
  * @array: pointer to array
  * @size: size of array
  * @action: function pointers
- * Return: nothing
+ * Return: void nothing
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int p;
+	size_t p;
 
-	if (array == NULL && action == NULL)
+	if (array == NULL || action == NULL)
 		return;
 
 	for (p = 0; p < size; p++)
