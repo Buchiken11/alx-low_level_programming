f6961af7f3eca2949de3c8bcb3fa3c60a6e1c04a
Buchiken11 <buchiken025@gmail.com>
2023-09-21 21:59:22 -0700

    nnn

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+This project contains the topic singly linked list,
+this is like a data type that can accommodate any amount of data being added to it.
+it's node is a pointer to the nsxt node equal to NULL
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,12 @@
+#ifndef LISTS_H
+#define LISTS_H
+ 
+list_t;
+
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+
+#endif
