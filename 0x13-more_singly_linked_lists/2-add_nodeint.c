294b5e0b733ab9247867bb2c5bcc8dd19749e83c
Buchiken11 <buchiken025@gmail.com>
2023-09-26 08:14:36 -0700

    task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * *add_nodeint-function add a node at the beginning of singly linked list node
+ * @head: head of singly liked list node
+ * @n: number of data in the list
+ * return: the pointer to the list
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	{
+		new->n = n;
+		new->next = *head;
+		*head = new;
+	}
+	return (new);
+}
