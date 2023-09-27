5b867edb42dd8d381b9750f77696e0c8ee08ddc8
Buchiken11 <buchiken025@gmail.com>
2023-09-26 19:59:06 -0700

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint-function frees the memory allocated to
+ * a singly liked list
+ * @head: the head of the sinhly linked list node
+ * Return: nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		current = head->next;
+		free(current);
+	}
+}
+
