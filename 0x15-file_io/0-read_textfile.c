536482a9732744130f6888f9516fe3c05a58f078
Buchiken11 <buchiken025@gmail.com>
2023-10-03 05:39:29 -0700

    updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *file;
 	ssize_t op_en;
 
-	fd = open(filename, O_RDONLY);
+	op_en = open(filename, O_RDONLY);
 	if (op_en == -1)
 		return (0);
 	file = malloc(sizeof(char) * letters);
