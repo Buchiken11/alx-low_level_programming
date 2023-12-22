07ae70a6f4536d102a153e9a3438ada49271d2b0
Buchiken11 <buchiken025@gmail.com>
2023-12-22 15:51:15 +0100

    task0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,36 +1,5 @@
-#include "hash_tales.h"
+#include "hash_tables.h"
 
-/**
- * hash_node_t - Node of a hash table
- * @key: The key, string
- * @value: The value corresponding to a key
- * @next: A pointer to the next node of the List
- */
-typedef struct hash_node_s
-{
-	char *key;
-	char *value;
-	struct hash_node_s *next;
-} hash_node_t;
-
-/**
- * hash_table_t - Hash table data structure
- * @size: The size of the array
- * @array: An array of size @size
- * Each cell of this array is a pointer to the first node of a linked list,
- * because we want our HashTable to use a Chaining collision handling
- */
-typedef struct hash_table_s
-{
-	unsigned long int size;
-	hash_node_t **array;
-} hash_table_t;
-
-/**
- * hash_table_create - Creates a hash table
- * @size: The size of the array
- * Return: A pointer to the newly created hash table, or NULL if something went wrong
- */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = NULL;
